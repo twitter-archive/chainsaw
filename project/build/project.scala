e8483c6dabe872c93fc6286f0a52428cd285abe7
Jeremy Cloud <jeremy.cloud@me.com>
2011-01-04 18:11:00 -0500

    switched to slf4j 1.5.8, because thrift 0.5 uses it

--- project/build/project.scala
+++ project/build/project.scala
@@ -4,7 +4,7 @@ import com.twitter.sbt._
 class ChainsawProject(info: ProjectInfo) extends StandardProject(info) with SubversionPublisher {
   override def subversionRepository = Some("http://svn.local.twitter.com/maven/")
 
-  val slf4jVersion = "1.6.1"
+  val slf4jVersion = "1.5.8"
   val slf4jApi = "org.slf4j" % "slf4j-api" % slf4jVersion withSources
   val specs = "org.scala-tools.testing" % "specs_2.8.1" % "1.6.6" % "test"
 }
